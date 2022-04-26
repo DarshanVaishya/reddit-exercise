e9f4dd9bd13d0a0b25216b3304356573d5593e0a
Darshan Vaishya <DarshanVaishya3@gmail.com>
2022-04-26 10:36:55 +0530

    Implemented input box for searching

--- src/App.js
+++ src/App.js
@@ -1,33 +1,42 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Post from "./components/Post";
 import { getPosts } from "./util/api";
 
 function App() {
 	const [posts, setPosts] = useState([]);
 	const [sort, setSort] = useState(false);
+	const [subreddit, setSubreddit] = useState(null);
+	const inputRef = useRef();
 
 	useEffect(() => {
-		const subreddit = prompt("Enter a subreddit name", "");
-		getPosts(subreddit || "reactjs").then((data) => setPosts(data));
-	}, []);
+		if (subreddit) getPosts(subreddit).then((data) => setPosts(data));
+	}, [subreddit]);
 
-	if (posts.length === 0) return <h3>Loading...</h3>;
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		console.log("XD");
+		setSubreddit(inputRef.current.value);
+	};
 
 	const toShow = sort ? posts.slice().sort((a, b) => b.score - a.score) : posts;
-	console.log(posts);
 	return (
 		<>
-			<button
-				style={{
-					padding: "5px",
-					margin: "5px",
-					background: "none",
-					border: "2px solid black",
-				}}
-				onClick={() => setSort(!sort)}
-			>
-				Sort
-			</button>
+			<form onSubmit={handleSubmit}>
+				<button
+					style={{
+						padding: "5px",
+						margin: "5px",
+						background: "none",
+						border: "2px solid black",
+					}}
+					type="button"
+					onClick={() => setSort(!sort)}
+				>
+					Sort
+				</button>
+				<input type="text" ref={inputRef} />
+			</form>
+
 			{toShow.map((post) => (
 				<Post key={post.id} post={post} />
 			))}
